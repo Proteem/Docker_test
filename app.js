6f928be427aed532b6968f3194468dc36c02a5b6
Proteem Ganguly <proteem.ganguly@p360.com>
2020-12-14 08:23:24 -0500

    node-app

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
+const NodeClam = require('clamscan');
+const ClamScan = new NodeClam().init();
 
 app.use(cors())
 app.use(fileUpload())
+
 
 app.get('/',(req,res)=>{
     res.send('welcome to upload handler<br>POST uploads to /upload as multipart-form')
@@ -24,4 +27,22 @@ app.post('/upload',(req,res)=>{
         }
         res.json({"uploaded": file.name})
     })
+    ClamScan.then(async clamscan => {
+        try {
+              // You can re-use the `clamscan` object as many times as you want
+              clamscan.is_infected(`${__dirname}/uploads/${file.name}`).then(result => {
+                  const {file, is_infected, viruses} =  result;
+                  if (is_infected) console.log(`${file} is infected with ${viruses.join(', ')}.`);
+                  else console.log("Sucess");
+              }).then(err => {
+                  console.error(err);
+              })
+          } catch (err) {
+              // Handle any errors raised by the code in the try block
+              console.error(err);
+          }
+      })
 })
+
+
+app.listen(port, ()=>console.log(`server started at ${port}`))
